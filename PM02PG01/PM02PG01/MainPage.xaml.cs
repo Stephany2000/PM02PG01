8a8b2632e7680a3c04798aea2c987ce4d2129c56
Stephany2000 <calixstephany2000@gmail.com>
2021-09-26 03:13:20 -0600

    Agregar archivos de proyecto.

---
+++ PM02PG01/PM02PG01/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PM02PG01
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void Btnagregar_Clicked(object sender, EventArgs e)
+        {
+            var num1 = Convert.ToDecimal(txtnum1.Text);
+            var num2 = Convert.ToDecimal(txtnum2.Text);
+         
+            await Navigation.PushAsync(new SecondPage(Convert.ToDecimal(num1 + num2)));
+           
+            //await DisplayAlert("SUMA",Convert.ToString(num1+num2),"OK");
+        }
+
+        private async void Btnresta_Clicked(object sender, EventArgs e)
+        {
+            var num1 = Convert.ToDecimal(txtnum1.Text);
+            var num2 = Convert.ToDecimal(txtnum2.Text);
+
+            await Navigation.PushAsync(new SecondPage(Convert.ToDecimal(num1 - num2)));
+
+            //await DisplayAlert("SUMA",Convert.ToString(num1+num2),"OK");
+        }
+
+        private async void Btnmultiplicacion_Clicked(object sender, EventArgs e)
+        {
+            var num1 = Convert.ToDecimal(txtnum1.Text);
+            var num2 = Convert.ToDecimal(txtnum2.Text);
+
+            await Navigation.PushAsync(new SecondPage(Convert.ToDecimal(num1 * num2)));
+
+            //await DisplayAlert("SUMA",Convert.ToString(num1+num2),"OK");
+        }
+
+        private async void Btndivision_Clicked(object sender, EventArgs e)
+        {
+            var num1 = Convert.ToDecimal(txtnum1.Text);
+            var num2 = Convert.ToDecimal(txtnum2.Text);
+
+            await Navigation.PushAsync(new SecondPage(Convert.ToDecimal(num1 / num2)));
+
+            //await DisplayAlert("SUMA",Convert.ToString(num1+num2),"OK");
+        }
+    }
+
+}
---
+++ PM02PG01/PM02PG01/Models/Personas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM02PG01.Models
+{
+    class Personas
+    {
+      
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public Int32 Edad { get; set; }
+        public string Correo { get; set; }
+
+    }
+}
